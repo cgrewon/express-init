5744da25b889aa6404ca54fc86cad42022358873
cgrewon <zhengyunxm@gmail.com>
2023-01-23 19:07:44 +0800

    INit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    const remoteAddress = req.connection.remoteAddress;
+    const remotePort = req.connection.remotePort;
+    const localAddress = req.connection.localAddress;
+    const localPort = req.connection.localPort;
+
+    res.send(`Hello World! Simple web server port:${localPort}, remote:${remoteAddress}, local:${localAddress}`);
+});
+
+app.listen(port, () => {
+    console.log(`Sample app listening on port ${port}`);
+});
